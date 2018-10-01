1837d4eb3bc1b2baf5e4b882382feba01eed1acb
Lars Ingebrigtsen <larsi@gnus.org>
2018-10-01 15:51:27 +0200

    Scaffolding

---
+++ ewp.el
@@ -0,0 +1,33 @@
+;;; ewp.el --- Interrogating Wordpress.com exports
+;; Copyright (C) 2018 Lars Magne Ingebrigtsen
+
+;; Author: Lars Magne Ingebrigtsen <larsi@gnus.org>
+;; Keywords: wordpress
+
+;; Ewp is free software; you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; Ewp is distributed in the hope that it will be useful, but WITHOUT
+;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+;; or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+;; License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with Ewp; see the file COPYING.  If not, write to the Free
+;; Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+;; 02111-1307, USA.
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'cl)
+(require 'dom)
+
+  
+
+(provide 'ewp)
+
+;;; ewp.el ends here
