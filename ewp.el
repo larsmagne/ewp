ec349fa89d8cfb1711c50ba5b6242000ed620a56
Lars Ingebrigtsen <larsi@gnus.org>
2020-08-31 00:05:48 +0200

    Use a class for the floating stuff instead of hard-coding

--- ewp.el
+++ ewp.el
@@ -1909,13 +1909,13 @@ If given a prefix, float to the right instead."
   (beginning-of-line)
   (let ((image (get-text-property (point) 'display)))
     (unless image
-      (error "No image under point to float left"))
-    (insert (format "<p style=\"clear: both;\"><p style=\"float: %s;\">"
+      (error "No image under point to float"))
+    (insert (format "<p style=\"clear: both;\"><p class=\"%s-img\">"
 		    (if right "right" "left")))
     (beginning-of-line)
     (put-text-property (point) (line-end-position)
 		       'display image))
-  (message "Floating image to the left"))
+  (message "Floating image to the %s" (if right "right" "left")))
 
 (defun ewp-crop-image ()
   "Crop the image under point."
